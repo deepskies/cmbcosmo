56375856b50f36eead643555eb333ec3690713ab
Humna Awan <humna.awan@rutgers.edu>
2024-01-09 16:07:01 -0500

    updated theory to use simcmb option to specify specific cls.

--- cmbcosmo/theory.py
+++ cmbcosmo/theory.py
@@ -25,16 +25,21 @@ class theory(object):
         * outdir: str or None
         
         """
+        # set up the keys we want
+        self.keys_of_interest = ['clTT', 'clEE', 'clBB', 'clEB']
         # load the default config in deepcmbsim and udpate some things
         self.config_obj = simcmb.config_obj()
         print(f'initial config: {self.config_obj.UserParams}\n')
         self.config_obj.update_val('max_l_use', lmax)
+        self.lmax = lmax
         self.verbose = verbose
         self.config_obj.update_val('verbose', int(self.verbose))
+        self.config_obj.update_val('cls_wanted', self.keys_of_interest)
         # now add detector noise
         if detector_noise:
             self.config_obj.update_val('noise_type', 'detector-white')
         self.outdir = outdir
+        self.data_tag = f'lmax{lmax}_{len(self.keys_of_interest)}spectra'
 
     # ---------------------------------------------
     def get_prediction(self, r, plot_things=False, plot_tag='',
@@ -51,6 +56,10 @@ class theory(object):
         """
         self.config_obj.update_val('InitPower.r', r, verbose=self.verbose)
         data = simcmb.CAMBPowerSpectrum(self.config_obj).get_cls()
+        # update data tag if not all keys of interest are a
+        if self.keys_of_interest + ['l'] != list(data.keys()):
+            self.data_tag = f'lmax{self.lmax}_{len(data.keys())-1}spectra'
+        # see if need to plot things
         if plot_things:
             if self.outdir is None:
                 raise ValueError('outdir much be set for plotting things.')
@@ -65,7 +74,7 @@ class theory(object):
             plt.ylabel(r'$C_\ell$')
             if plot_tag != '':
                 plot_tag = '_' + plot_tag
-            fname = f'plot_cls{plot_tag}.png'
+            fname = f'plot_cls{plot_tag}_{self.data_tag}.png'
             plt.savefig(f'{self.outdir}/{fname}',
                         bbox_inches='tight', format='png')
             print('# saved %s' % fname)
